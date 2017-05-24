1487fa41c617fbf639c11b07129762d8ef487775
soxface <sonyacorcoran@gmail.com>
2017-05-24 14:08:42 +1000

    learning simple pug syntax vid5

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const sox = { name: 'Sox', age: 100, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(sox); // displays json on client
+  // res.send(req.query.name); // displays html from params
+  // res.json(req.query); // displays json from params
+  res.render('hello', {
+    name: 'Sox',
+    dog: 'Bay'
+  });
 });
 
 module.exports = router;
