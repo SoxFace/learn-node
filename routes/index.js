34e13d4d83b40c49b3f9a14b8c9ac6b211d78959
soxface <sonyacorcoran@gmail.com>
2017-05-26 14:45:13 +1000

    v7 created storeController to render index

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController =require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const sox = { name: 'Sox', age: 100, cool: true };
-  // res.send('Hey! It works!');
-  // res.json(sox); // displays json on client
-  // res.send(req.query.name); // displays html from params
-  // res.json(req.query); // displays json from params
-  res.render('hello', {
-    name: 'Sox',
-    dog: 'Bay'
-  });
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
